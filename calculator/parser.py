3da9ea17c646800ebbe427343673e5b88aa13afd
Jimarious <jimaras.jr@gmail.com>
2026-09-14 19:20:14 +0300

    Fixed more basic bugs

--- calculator/parser.py
+++ calculator/parser.py
@@ -30,20 +30,25 @@ class Parser:
         return root
 
     def _parse_sum(self):
+        print("_parse_sum     | token index:", self._token_idx, "| peek:", self._peek())
         node = self._parse_product()
-        while (operator := self._peek) in ['+','-']:
+        print("_parse_sum: just a peek", self._peek())
+        while (operator := self._peek()) in ['+','-']:
             self._token_idx += 1
-            node = Operation(operator, node, self._parse_product)
+            print("_parse_sum: just a peek 2", self._peek())
+            node = Operation(operator, node, self._parse_product())
         return node
 
     def _parse_product(self):
+        print("_parse_product     | token index:", self._token_idx, "| peek:", self._peek())
         node = self._parse_atom()
-        while (operator := self._peek) in ['*','/']:
+        while (operator := self._peek()) in ['*','/']:
             self._token_idx += 1
-            node = Operation(operator, node, self._parse_atom)
+            node = Operation(operator, node, self._parse_atom())
         return node
 
     def _parse_atom(self):
+        print("_parse_atom     | token index:", self._token_idx, "| peek:", self._peek())
         if (token := self._take()) == '(':
             node = self._parse_sum()
             if self.take() == ')': return node
